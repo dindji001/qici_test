1f7a526fa004e4987bf8f34cb33e73b1f17d4cfb
Severin Dindji <severindindji@192.168.1.11>
2024-03-02 10:56:56 +0000

    mettre a jour le projet

--- lib/view/screens/inviter/invite-collaborateur-page.dart
+++ lib/view/screens/inviter/invite-collaborateur-page.dart
@@ -10,30 +10,88 @@ class InviteCollaborateurPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'Inviter un utilisateur',
-          style: TextStyle(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'Inviter un utilisateur',
+            style: TextStyle(
+              color: AppTheme.color.whithColor,
+            ),
+          ),
+          actions: [Icon(Icons.add)],
+          iconTheme: IconThemeData(
             color: AppTheme.color.whithColor,
           ),
+          backgroundColor: AppTheme.color.primaryColor,
         ),
-        iconTheme: IconThemeData(
-          color: AppTheme.color.whithColor,
-        ),
-        backgroundColor: AppTheme.color.primaryColor,
-      ),
-      body: Center(
-        child: CButton(
-          title: 'Aller Precommander',
-          onPressed: () {
-            route(
-              context,
-              Listeprecommande(),
-            );
-          },
-        ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Invité un collaborateur',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Entrer l’adresse email de l’utilisateur que vous souhater envoyer l’invitations',
+                    style: TextStyle(
+                      color: AppTheme.color.backgroundTextFieldBordu,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Color(0XffD9D9D9),
+                      ),
+                      filled: true,
+                      hintText: "Email",
+                      hintStyle: TextStyle(
+                        color: Color(0XffD9D9D9),
+                        fontSize: 14,
+                      ),
+                      fillColor: AppTheme.color.backgroundTextField,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 15,
+                      ),
+                    ),
+                    onChanged: (value) {},
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Column(
+                children: [
+                  Text(
+                    'Attribution des Tâches',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
